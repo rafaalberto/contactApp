e35accf7d780324aacedaa46ff72cb1676929dda
Rafael Alberto <rafael.alberto1703@gmail.com>
2020-05-31 20:26:54 -0300

    apply data binding

--- app/src/main/java/br/com/contact/app/activity/ContactListActivity.kt
+++ app/src/main/java/br/com/contact/app/activity/ContactListActivity.kt
@@ -2,13 +2,17 @@ package br.com.contact.app.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import br.com.contact.R
+import br.com.contact.databinding.ActivityContactListBinding
 
 class ContactListActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityContactListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contact_list)
     }
 
 }
